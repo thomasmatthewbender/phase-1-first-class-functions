5f3920df8a4eb1e97306b65ba0c921162843eae1
Thomas Bender <thomasmatthewbender@gmail.com>
2022-03-18 09:30:07 -0400

    commit tree 3cad4e4f2cbbac09c09c97f99ed0b40c5820774f

---
+++ index.js
@@ -0,0 +1,7 @@
+const receivesAFunction = () => alert('sup')
+
+setTimeout(receivesAFunction, 3000)
+
+const returnsANamedFunction = () => "_named function_"
+
+const returnsAnAnonymousFunction = () => "_anonymous function_"
